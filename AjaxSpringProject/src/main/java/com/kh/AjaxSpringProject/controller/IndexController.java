0fb3a6e6b9407dba4614cbaf94953dec2702d58c
coiilee <dakyung96@gmail.com>
2025-01-16 16:12:15 +0900

    api pre

--- AjaxSpringProject/src/main/java/com/kh/AjaxSpringProject/controller/IndexController.java
+++ AjaxSpringProject/src/main/java/com/kh/AjaxSpringProject/controller/IndexController.java
@@ -5,10 +5,7 @@ import com.kh.AjaxSpringProject.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class IndexController {
@@ -22,8 +19,8 @@ public class IndexController {
 
     // Ajax 와 별개로 /detail/원하는회원정보번호를 통해서
     // userInfo.html을 보여줄 수 있도록 설정하는 controller
-    @GetMapping("/detail/{id}")
-    public String detail(@PathVariable int id, Model model) {
+    @GetMapping("/detail")
+    public String detail(@RequestParam("id")int id, Model model) {
         model.addAttribute("user",userService.getUserById(id));
         return "userInfo";
     }
--- AjaxSpringProject/src/main/java/com/kh/AjaxSpringProject/mapper/CafeMapper.java
+++ AjaxSpringProject/src/main/java/com/kh/AjaxSpringProject/mapper/CafeMapper.java
@@ -7,6 +7,8 @@ import java.util.List;
 
 @Mapper
 public interface CafeMapper {
+    //카페 목록 전체 조회 [전체] or [목록] or [리스트]를 작성하게 되면
+    //List<DTO파일명> mapper.xml에서 작성한 id 명칭;
 
     List<Cafe> getAllCafes();
 
