855dd3eb99ea9f3a5fc701f7fc790912db5a2999
coiilee <dakyung96@gmail.com>
2025-01-17 15:31:12 +0900

    window.location.search, URLSearchParams

--- AjaxSpringProject/src/main/java/com/kh/AjaxSpringProject/controller/CafeController.java
+++ AjaxSpringProject/src/main/java/com/kh/AjaxSpringProject/controller/CafeController.java
@@ -1,17 +1,8 @@
 package com.kh.AjaxSpringProject.controller;
 
-import com.kh.AjaxSpringProject.dto.Cafe;
-import com.kh.AjaxSpringProject.service.CafeService;
-import com.kh.AjaxSpringProject.service.CafeServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @Controller
 public class CafeController {
@@ -19,13 +10,13 @@ public class CafeController {
 
     @GetMapping("/cafe")
     public String cafe() {
-        return "cafe";
+        return "/jQueryAjax/cafe";
     }
 
     //사이트 주소 접근용 !!
-    @GetMapping("/cafe/detail")
+    @GetMapping("cafe/detail")
     public String detail(@RequestParam("id") int id) {
-        return "cafeList";
+        return "/jQueryAjax/cafeList";
     }
 
 }
